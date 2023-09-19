1027fcbadff0d053e952a4a5c90e908ceb998571
Sam Black <sam.black@lubbockisd.org>
2023-09-19 09:39:09 -0500

    Completed Day 01 part 2

--- src/Day01a.java
+++ src/Day01a.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Day01a {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day01a {
+
+    public static void main(String[]args) throws Throwable{
+        Scanner infile = new Scanner(new File("day01.dat"));
+        ArrayList<Integer> values = new ArrayList<>();
+        while(infile.hasNext()){
+            values.add(infile.nextInt());
+        }
+
+        int curr = -1, sum = 0, i=0;
+        while(sum != 2020){
+            curr++;
+            for( i = curr + 1; i < values.size(); i++){
+                sum = values.get(curr) + values.get(i);
+                if(sum == 2020)
+                    break;
+            }
+        }
+        System.out.println(values.get(curr)*values.get(i));
+
+    }
 }
--- src/Day01b.java
+++ src/Day01b.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Day01b {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day01b {
+
+    public static void main(String[] args) throws Throwable {
+        Scanner infile = new Scanner(new File("day01.dat"));
+        ArrayList<Integer> values = new ArrayList<>();
+        while (infile.hasNext()) {
+            values.add(infile.nextInt());
+        }
+
+        int curr = -1, sum = 0, i = 0, j = 0;
+        while (sum != 2020) {
+            curr++;
+            for (i = curr + 1; sum !=2020 && i < values.size(); i++)
+                for (j = i + 1; j < values.size(); j++) {
+                    sum = values.get(curr) + values.get(i) + values.get(j);
+                    if (sum == 2020){
+                        System.out.println("woot");
+                        break;
+                    }
+
+                }
+        }
+        i--;
+        System.out.println(values.get(curr));
+        System.out.println(values.get(i));
+        System.out.println(values.get(j));
+
+        long prod = (long)values.get(curr) * (long)values.get(i) * (long)values.get(j);
+        System.out.println(prod);
+
+    }
 }
