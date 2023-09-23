6ea949a6b97233dd93c14515f24f5fe54adc5579
Sam Black <sam.black@lubbockisd.org>
2023-09-23 18:16:35 -0500

    Day 05 part 1 set up and ready to code

---
+++ src/DayXX.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class DayXX {
+    public static void main(String[]args) throws Throwable{
+
+        Scanner infile = new Scanner(new File("day05.dat"));
+        ArrayList<String> values = new ArrayList<>();
+        while(infile.hasNext()){
+            values.add(infile.next());
+        }
+
+        int bigID = -1;
+        for(String seat:values){
+// process boarding codes into seat IDs
+
+        }
+        System.out.println("Answer: " + bigID);
+
+    }
+}
+
+
