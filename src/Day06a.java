3fa0a9fcf0f460674bd836a698683724bd7d70fe
Sam Black <sam.black@lubbockisd.org>
2023-10-03 17:54:27 -0500

    Day 06 part 1 completed in 06a and part 2 in process

---
+++ src/Day06a.java
@@ -0,0 +1,48 @@
+import java.io.File;
+import java.lang.reflect.Array;
+import java.util.*;
+
+import static java.lang.System.out;
+
+public class Day06a {
+    public static void main(String[] args) throws Throwable {
+        Scanner infile = new Scanner(new File("day06.dat"));
+        ArrayList<ArrayList<String>> values = new ArrayList<>();
+        ArrayList<String> group = new ArrayList<>();
+        while (infile.hasNextLine()) {
+            String line = infile.nextLine();
+            group.add(line);
+            if (line.isEmpty()) {
+                values.add(group);
+                group.clear();
+            }
+        }
+        values.add(group);
+
+
+        int sum = 0;
+        for (ArrayList<String> grp : values) {
+            String first = grp.get(0);
+            boolean found = true;
+            int count = 0;
+            while(found){
+                String[] fst = first.split("");
+                int curr = 0;
+                for(String s: grp){
+                    if(!s.contains(fst[curr])){
+                        found = false;
+
+                    }
+                }
+            }
+
+
+            sum += questions.size();
+        }
+
+        out.println("Answer: " + sum);
+
+
+    }
+}
+
