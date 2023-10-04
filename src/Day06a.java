c2e66682a1b35dae95e24b1357a0fa8d1ccacca0
Sam Black <sam.black@lubbockisd.org>
2023-10-04 15:04:35 -0500

    Day 06 part 1 and 2 completed in 06a

--- src/Day06a.java
+++ src/Day06a.java
@@ -11,15 +11,19 @@ public class Day06a {
         ArrayList<String> group = new ArrayList<>();
         while (infile.hasNextLine()) {
             String line = infile.nextLine();
-            group.add(line);
-            if (line.isEmpty()) {
+            if(!line.isEmpty())
+                group.add(line);
+            else {
                 values.add(group);
-                group.clear();
+ //               out.println(group);
+                group = new ArrayList<>();
             }
+ //           out.println(values);
         }
         values.add(group);
 
 
+//        out.println(values);
         int sum = getSum(values);
 
         out.println("Answer: " + sum);
@@ -30,25 +34,39 @@ public class Day06a {
     private static int getSum(ArrayList<ArrayList<String>> values) {
         int sum = 0;
         for (ArrayList<String> grp : values) {
-            String first = grp.get(0);
-            boolean found = true;
+            out.println(grp);
+            String longest = getLongest(grp);
             int count = 0;
-            while(found){
-                String[] fst = first.split("");
-                int curr = 0;
-                for(String s: grp){
-                    if (!s.contains(fst[curr])) {
-                        found = false;
-
+            String[] fst = longest.split("");
+            for(int curr = 0; curr < longest.length(); curr++){
+                int i;
+                for(i = 0; i<grp.size();i++) {
+                    out.println(longest + " " + grp.get(i));
+                    if (!grp.get(i).contains(fst[curr])) {
                         break;
                     }
                 }
+                if(i<grp.size()) {
+                    out.println("abort");
+                    continue;
+                }
+                count++;
+                out.println("curr:" +curr + " count: "+count);
+
             }
-
-
             sum += count;
+            out.println("sum: "+sum +" count: "+count);
         }
         return sum;
+    }
+
+    public static String getLongest(ArrayList<String> list){
+        String longest = list.get(0);
+        for(String s: list)
+            if(s.length()>longest.length()) {
+                longest = s;
+            }
+        return longest;
     }
 }
 
