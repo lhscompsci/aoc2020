e5c83ba453d52f08ad87cd802aaf990a05a45e3b
Sam Black <samblack@gmail.com>
2023-10-04 17:50:09 -0500

    7a in process

--- src/Day07a.java
+++ src/Day07a.java
@@ -7,15 +7,20 @@ public class Day07a {
 
         Scanner infile = new Scanner(new File("small.dat"));
         ArrayList<String> values = new ArrayList<>();
+        ArrayList<Bag> bags = new ArrayList<>();
         while(infile.hasNext()){
-            values.add(infile.next());
+            values.add(infile.nextLine());
         }
 
         int bigID = -1;
-        for(String seat:values){
-// process boarding codes into seat IDs
-
+        for(String val:values){
+            String[] breaker = val.split(" bags contain ");
+            Bag b = new Bag(breaker[0],breaker[1]);
+            bags.add(b);
         }
+
+
+
         System.out.println("Answer: " + bigID);
 
     }
@@ -26,15 +31,23 @@ class Bag {
     private String color;
     private ArrayList<Bag> list;
 
-    public Bag(String c){
+    public Bag(String c, String l){
         color = c;
         list = new ArrayList<>();
+        addBags(l);
     }
 
     public void addBags(String l){
         String[] bags = l.split(", ");
         if(bags[0].contains("no other"))
             return;
-
+        for(String b:bags){
+            String[] breakdown = b.split(" ");
+            String ba = breakdown[1]+ " " + breakdown[2];
+            list.add(new Bag(ba,""));
+        }
+    }
+    public String getColor(){
+        return color;
     }
 }
