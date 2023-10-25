3b2b0e361861663ce4f2b28012b0fe84d667f003
Sam Black <sam.black@lubbockisd.org>
2023-10-25 18:09:15 -0500

    Day 08 Part B is in progress.

--- src/Day08b.java
+++ src/Day08b.java
@@ -2,10 +2,10 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Day08a {
+public class Day08b {
     public static void main(String[]args) throws Throwable{
 
-        Scanner infile = new Scanner(new File("day08.dat"));
+        Scanner infile = new Scanner(new File("small8.dat"));
         ArrayList<Integer> steps = new ArrayList<>();
         ArrayList<Cmd> cmds = new ArrayList<>();
         while(infile.hasNext()){
@@ -17,16 +17,25 @@ public class Day08a {
         int sum = 0;
         int step = 0;
 
-        while(!steps.contains(step)){
-            steps.add(step);
+
+        while(step<cmds.size()){
+
+            if(steps.contains(step)) {
+                step= steps.get(steps.size()-1) +1;
+
+            }
+            else
+                steps.add(step);
             switch(cmds.get(step).getCmd()){
                 case "acc": sum += cmds.get(step).getVal();
-                            step++;
-                            break;
+                    step++;
+                    break;
                 case "jmp": step += cmds.get(step).getVal();
-                            break;
+                    break;
                 case "nop": step++;
             }
+
+
         }
 
 
@@ -35,20 +44,25 @@ public class Day08a {
     }
 }
 
-//
-//class Cmd {
-//    private final String cmd;
-//    private final int val;
-//
-//    public Cmd(String c, int v){
-//        cmd = c;
-//        val = v;
-//    }
-//
-//    public int getVal(){
-//        return val;
-//    }
-//    public String getCmd(){
-//        return cmd;
-//    }
-//}
+
+class Cmd {
+    private String cmd;
+    private final int val;
+
+    public Cmd(String c, int v){
+        cmd = c;
+        val = v;
+    }
+
+    public void setCmd(String cmd) {
+        this.cmd = cmd;
+    }
+
+
+    public int getVal(){
+        return val;
+    }
+    public String getCmd(){
+        return cmd;
+    }
+}
